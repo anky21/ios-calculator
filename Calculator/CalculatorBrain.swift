eadfac7455a1f648d11cfd90877c058a44a51bd1
anky21 <anky25@gmail.com>
2017-06-13 21:31:06 +1000

    feat: Use enumerations to work with constants and unary operations

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,18 +8,37 @@
 
 import Foundation
 
+func changeSign(operand: Double) -> Double{
+    return -operand
+}
+
 struct CalculatorBrain {
     
     private var accumulator: Double?
     
-    private var operations: Dictionary<String, Double> = [
-        "π": Double.pi,
-        "e": M_E
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "±": Operation.unaryOperation(changeSign)
     ]
     
     mutating func performOperation(_ symbol: String) {
-        if let constant = operations[symbol]{
-            accumulator = constant
+        if let operation = operations[symbol]{
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil{
+                    accumulator = function(accumulator!)
+                }
+            }
         }
     }
     
